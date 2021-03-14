97e60331b9d43fce0a692469968de12881745473
Ermeso <sampaioermeson95@gmail.com>
2021-03-14 18:34:52 -0300

    navbar responsivo

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+function createModal() {
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  modal.classList.add('disable');
+  
+  const buttonClose = document.createElement('div');
+  buttonClose.innerHTML = `
+    <p>X</p>
+  `;
+  
+  buttonClose.classList.add('button-close')
+  modal.appendChild(buttonClose);
+  modal.appendChild(document.querySelector('nav ul'));
+
+  document.querySelector('body').appendChild(modal);
+}
+
+function openModal() {
+  document.querySelector('.modal').classList.remove('disable');
+  
+  document.querySelector('.button-close').addEventListener('click', closeModal)
+}
+
+function closeModal() {
+  document.querySelector('.modal').classList.add('disable');
+}
+
+if(window.innerWidth <= 800) {
+  const burger = document.createElement('div');
+  burger.innerHTML = `
+    <div class="lines"></div>
+    <div class="lines"></div>
+    <div class="lines"></div>
+  `
+  burger.classList.add('burger');
+
+  document.querySelector('.container-menu').classList.add('ul-modal');
+  
+  document.querySelector('.container-menu').appendChild(burger);
+  
+  createModal();
+  burger.addEventListener('click', openModal);
+}
